81bded0f3d7fb50a4450c5018535ff338829ade7
Rishabh Choudhary <rishabhchoudhary140126@gmail.com>
2026-06-23 21:49:17 +0530

    Time: 0 ms (100%), Space: 22.5 MB (6.97%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector <int> freq(nums.size()+1, 0);
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+        for(int i=0; i<freq.size(); i++){
+            if(freq[i]==0) return i;
+        }
+        return -1;
+    }
+};
